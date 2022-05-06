9e611bcda96213cec813fdb43f2edf882c2c01c5
Joao Ladeira <joaoladeirag@gmail.com>
2022-05-06 17:57:11 -0300

    feat: add InfiniteScroller component

---
+++ src/components/InfiniteScroller/index.tsx
@@ -0,0 +1,50 @@
+import React, { ReactNode, useCallback, useEffect, useRef } from 'react';
+import styles from './infiniteScroller.module.scss';
+
+interface InfiniteScrollerProps {
+  onEndReached: () => void;
+  isLoading?: boolean;
+  children: ReactNode;
+}
+
+export const InfiniteScroller = ({
+  onEndReached,
+  isLoading = false,
+  children,
+}: InfiniteScrollerProps) => {
+  const loader = useRef(null);
+  let isFetching = false;
+
+  const handleObserver = useCallback((entries: any) => {
+    const target = entries[0];
+
+    if (target.isIntersecting && !isFetching) {
+      isFetching = true;
+      onEndReached();
+      setTimeout(() => {
+        isFetching = false;
+      }, 300);
+    }
+  }, []);
+
+  useEffect(() => {
+    const option = {
+      root: null,
+      rootMargin: '2px',
+      threshold: 0,
+    };
+    const observer = new IntersectionObserver(handleObserver, option);
+
+    if (loader.current) observer.observe(loader.current);
+
+    return () => observer.disconnect();
+  }, [handleObserver]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.container}>{children}</div>
+      {isLoading && <p>Loading...</p>}
+      <div ref={loader} className="a" />
+    </div>
+  );
+};
