8a91b5830c04e773b45eb4fbcc7ec2119fa878c7
Joao Ladeira <joaoladeirag@gmail.com>
2022-05-06 19:42:05 -0300

    feat: add input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,26 @@
+import { ChangeEventHandler } from 'react';
+import styles from './Input.module.scss';
+
+interface InputProps {
+  type?: string;
+  placeholder?: string;
+  value: string | number;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+}
+
+export const Input = ({
+  type = 'text',
+  placeholder,
+  value,
+  onChange,
+}: InputProps) => {
+  return (
+    <input
+      className={styles.input}
+      placeholder={placeholder}
+      type={type}
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
