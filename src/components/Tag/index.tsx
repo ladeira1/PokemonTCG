bb502677abc144e84489653ca3178a5eb5f9df1c
Joao Ladeira <joaoladeirag@gmail.com>
2022-05-07 16:28:41 -0300

    style: Add value to Tag

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -3,15 +3,24 @@ import styles from './Tag.module.scss';
 
 interface TagProps {
   type: PokemonType;
+  value?: string;
+  valueColor?: 'Resistance' | 'Weakness';
 }
 
-export const Tag = ({ type }: TagProps) => {
+export const Tag = ({ type, value, valueColor }: TagProps) => {
   return (
-    <div
-      data-testid="tag"
-      className={`${styles.tag} ${styles[`color-${type}`]}`}
-    >
-      <p>{type}</p>
+    <div className={styles.container}>
+      <div
+        data-testid="tag"
+        className={`${styles.tag} ${styles[`color-${type}`]}`}
+      >
+        {value && (
+          <span className={valueColor && styles[`value-${valueColor}`]}>
+            {value}
+          </span>
+        )}
+        <p>{type}</p>
+      </div>
     </div>
   );
 };
