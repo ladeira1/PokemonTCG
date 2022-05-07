609fb5224e65cf1450ec6eb64a14a0ae16015673
Joao Ladeira <joaoladeirag@gmail.com>
2022-05-07 11:34:00 -0300

    feat: Add anchor to Small card

--- src/components/SmallCard/index.tsx
+++ src/components/SmallCard/index.tsx
@@ -2,6 +2,7 @@ import { TagList } from 'components/Tag/TagList';
 import { useTranslation } from 'hooks/useTranslation';
 import { smallCardTranslations } from 'translations/smallCard';
 import { Card } from 'types/Card';
+import Link from 'next/link';
 import styles from './SmallCard.module.scss';
 
 interface SmallCardProps {
@@ -13,22 +14,26 @@ export const SmallCard = ({ card }: SmallCardProps) => {
 
   return (
     <article data-testid="small-card" className={styles.card}>
-      <div>
-        <img
-          loading="lazy"
-          src={card.images.small}
-          alt={`${imageAlt} ${card.name}`}
-        />
-      </div>
+      <Link passHref href={`/card/${card.id}`}>
+        <a className={styles.link}>
+          <div>
+            <img
+              loading="lazy"
+              src={card.images.small}
+              alt={`${imageAlt} ${card.name}`}
+            />
+          </div>
 
-      <section className={styles.title} aria-labelledby={card.name}>
-        <div className="row">
-          <h2 id={card.name}>{card.name}</h2>
-          <p className={styles.id}>{card.id}</p>
-        </div>
+          <section className={styles.title} aria-labelledby={card.name}>
+            <div className="row">
+              <h2 id={card.name}>{card.name}</h2>
+              <p className={styles.id}>{card.id}</p>
+            </div>
 
-        <TagList tags={card?.types} />
-      </section>
+            <TagList tags={card?.types} />
+          </section>
+        </a>
+      </Link>
     </article>
   );
 };
